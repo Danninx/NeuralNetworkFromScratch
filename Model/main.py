48a14e9b42fb75ae81636c17c0bafc78e377c486
Danninx <danrfaj@gmail.com>
2023-12-22 21:32:11 -0600

    P2.1 Single layer iterations

--- Model/main.py
+++ Model/main.py
@@ -1,7 +1,21 @@
 # Single Neuron
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1, 2.1, 8.7]
-bias = 3
+inputs = [1, 2, 3, 2.5]
+weights = [
+    [0.2, 0.8, -0.5, 1.0],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87]
+]
+bias = [
+    2,
+    3,
+    0.5
+]
 
-output = sum([inputs[i]*weights[i] for i in range(len(inputs))]) + bias
+output = []
+
+for i in range(len(weights)):
+    w = weights[i]
+    b = bias[i]
+    output.append(sum([inputs[j]*w[j] for j in range(len(inputs))]) + b)
+
 print(output)
