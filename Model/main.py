c1d31bed4b4f046cd3d3ec345acd761e11fce956
Danninx <danrfja@gmail.com>
2023-12-25 22:20:48 -0600

    4.2 layer forward object

--- Model/main.py
+++ Model/main.py
@@ -1,31 +1,25 @@
 import numpy as np
 
-inputs = [
+np.random.seed(0)  # for random weight values
+X = [
     [1.0, 2.0, 3.0, 2.5],
     [2.0, 5.0, -1.0, 2.0],
     [-1.5, 2.7, 3.3, -0.8]
 ]
-weights = [
-    [0.2, 0.8, -0.5, 1.0],
-    [0.5, -0.91, 0.26, -0.5],
-    [-0.26, -0.27, 0.17, 0.87]
-]
-bias = [
-    2,
-    3,
-    0.5
-]
-weights2 = [
-    [0.1, -0.14, 0.5],
-    [-0.5, 0.12, -0.33],
-    [-0.44, 0.73, -0.13]
-]
-bias2 = [
-    -1,
-    2,
-    0.5
-]
 
-layer1_out = np.dot(inputs, np.array(weights).T) + bias
-layer2_out = np.dot(layer1_out, np.array(weights2).T) + bias2
-print(layer2_out)
+
+class LayerDense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.out = np.dot(inputs, self.weights) + self.biases
+
+layer1 = LayerDense(4,7)
+layer2 = LayerDense(7,1)
+
+layer1.forward(X)
+layer2.forward(layer1.out)
+
+print(layer2.out)
