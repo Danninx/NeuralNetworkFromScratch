10a8822f787806a1064b2891789385832256fced
Danninx <danrfja@gmail.com>
2023-12-25 22:10:17 -0600

    4.1 multi layer intuition

--- Model/main.py
+++ Model/main.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-inputs = [1.0, 2.0, 3.0, 2.5]
+inputs = [
+    [1.0, 2.0, 3.0, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]
+]
 weights = [
     [0.2, 0.8, -0.5, 1.0],
     [0.5, -0.91, 0.26, -0.5],
@@ -11,6 +15,17 @@ bias = [
     3,
     0.5
 ]
+weights2 = [
+    [0.1, -0.14, 0.5],
+    [-0.5, 0.12, -0.33],
+    [-0.44, 0.73, -0.13]
+]
+bias2 = [
+    -1,
+    2,
+    0.5
+]
 
-output = np.dot(weights, inputs) + bias
-print(output)
+layer1_out = np.dot(inputs, np.array(weights).T) + bias
+layer2_out = np.dot(layer1_out, np.array(weights2).T) + bias2
+print(layer2_out)
